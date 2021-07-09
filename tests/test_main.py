8eda80425aafa0e5c711781a9f33064f517d6ad2
Demenev Viktor <viktor.demen@gmail.com>
2021-07-09 21:58:53 +0300

    Fixed noqa formatting

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,4 +4,4 @@ from application.main import main
 def test_launch() -> None:
     """This is the example test that shows that package initialises without errors"""
     result = main()
-    assert result == "Hello World!" # noqa S101
+    assert result == "Hello World!"  # noqa: S101
