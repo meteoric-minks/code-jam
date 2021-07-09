d8e27bee6dd9764368ef963f7c711ca3d1a4a239
Demenev Viktor <viktor.demen@gmail.com>
2021-07-09 21:42:45 +0300

    Tweak to docstring

--- src/application/main.py
+++ src/application/main.py
@@ -2,9 +2,7 @@ import blessed
 
 
 def main() -> str:
-    """
-    This is a start of application's structure
-    """
+    """This is a start of application's structure"""
     term = blessed.Terminal()
     return "Hello World!"
 
