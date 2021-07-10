1185c417270db3afcb88bea2dd19598aade574a6
hecent <henrytchandler@outlook.com>
2021-07-10 17:14:30 +0100

    Added new functions to the vector module

--- src/vector.py
+++ src/vector.py
@@ -1,7 +1,10 @@
+from math import atan, cos, radians, sin
+
+
 class vector:
     """Basic 2D vectors for making more readable code"""
 
-    def __init__(self, x: int, y: int) -> None:
+    def __init__(self, x=0, y=0) -> None:
         self.x = x
         self.y = y
 
@@ -9,12 +12,32 @@ class vector:
         self.x += other.x
         self.y += other.y
 
+    def __sub__(self, other):  # noqa: ANN001
+        self.x -= other.x
+        self.y -= other.y
+
     def size(self) -> float:
         """Calculates the hypotemuse of a vector"""
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
+    def rotate(self, angle: float) -> None:
+        """Rotates the vector clockwise around the origin (angle should be in degrees)"""
+        angle = radians(angle)
+        s = sin(angle)
+        c = cos(angle)
+        newVector = vector()
+        newVector.x = (self.x * c) - (self.y * s)
+        newVector.y = (self.x * s) + (self.y * c)
+        self = newVector
 
-def distance(vectorA: vector(), vectorB: vector()) -> float:
+
+def vectorDistance(vectorA: vector(), vectorB: vector()) -> float:
     """Calculates the distance between two vectors"""
     finalVector = vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
     return finalVector.size()
+
+
+def vectorBearing(vectorA: vector(), vectorB: vector()) -> float:
+    """Calculates the bearing of vectorB from vectorA"""
+    newVector = vectorB-vectorA
+    return atan(newVector.x / newVector.y)
