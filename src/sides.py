f9adef9cb7dac5deb2a7b9a574cdd0fddef29280
hecent <henrytchandler@outlook.com>
2021-07-10 17:15:22 +0100

    Made getScreenLocation function in sides.py

--- src/sides.py
+++ src/sides.py
@@ -1,4 +1,4 @@
-from vector import vector
+from vector import vector, vectorBearing, vectorDistance
 
 
 class side:
@@ -19,3 +19,19 @@ class side:
         self.cornerA += translationVector
         self.cornerB += translationVector
         self.extremesCalc()
+
+    def getScreenLocation(self, cameraLocation: vector(), cameraRotation: float, fov: int) -> list:
+        """Returns the location on screen and the distance to the corners of the side"""
+        output = [[0, 0], [0, 0]]
+        cornerA = self.cornerA
+        cornerB = self.cornerB
+        cornerA -= cameraLocation
+        cornerB -= cameraLocation
+        cornerA.rotate(-cameraRotation)
+        cornerB.rotate(-cameraRotation)
+        cornerA += cameraLocation
+        cornerB += cameraLocation
+        output[0][0] = vectorBearing(cameraLocation, cornerA) / (fov / 2)
+        output[1][0] = vectorBearing(cameraLocation, cornerB) / (fov / 2)
+        output[0][1] = vectorDistance(cornerA, cameraLocation)
+        output[0][1] = vectorDistance(cornerB, cameraLocation)
