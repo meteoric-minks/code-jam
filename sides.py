25a9353116fd6b6e16fd05a775c82c9272eaaa8d
hecent <henrytchandler@outlook.com>
2021-07-10 12:08:57 +0100

    Updated Linting from the template repo

--- sides.py
+++ sides.py
@@ -7,12 +7,12 @@ class side:
         self.extremesCalc()
 
     def extremesCalc(self):
-        #gets the min and max point of the side > this is just for convenience later on
+        """gets the min and max point of the side"""
         self.mins = vector(min(self.cornerA.x,self.cornerB.x),min(self.cornerA.y,self.cornerB.y))
         self.maxs = vector(max(self.cornerA.x,self.cornerB.x),max(self.cornerA.y,self.cornerB.y))
 
-    #use to translate a side
     def move(self,translationVector):
+        """Translates a side along a given vector"""
         self.cornerA+=translationVector
         self.cornerB+=translationVector
         self.extremesCalc()
