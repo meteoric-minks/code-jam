01fa868a5014211d921258c8db53ffc38429346d
hecent <henrytchandler@outlook.com>
2021-07-10 19:33:31 +0100

    Fixed EOF error

--- src/wall.py
+++ src/wall.py
@@ -16,7 +16,7 @@ class wall:
         self.mins = vector(min(self.cornerA.x, self.cornerB.x), min(self.cornerA.y, self.cornerB.y))
         self.maxs = vector(max(self.cornerA.x, self.cornerB.x), max(self.cornerA.y, self.cornerB.y))
 
-    def move(self, translationVector: vector -> None:
+    def move(self, translationVector: vector) -> None:
         """Translates a wall along a given vector"""
         self.cornerA += translationVector
         self.cornerB += translationVector
