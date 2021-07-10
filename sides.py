862e623ba81029f924575fef225baab67f5a0003
hecent <henrytchandler@outlook.com>
2021-07-10 12:15:10 +0100

    Linting files

--- sides.py
+++ sides.py
@@ -1,18 +1,20 @@
 from vector import vector
 
 class side:
-    def __init__(self,cornerA,cornerB):
-        self.cornerA=cornerA
-        self.cornerB=cornerB
+    """side objects to make up the geometry of the world"""
+
+    def __init__(self, cornerA, cornerB):
+        self.cornerA = cornerA
+        self.cornerB = cornerB
         self.extremesCalc()
 
     def extremesCalc(self):
         """gets the min and max point of the side"""
-        self.mins = vector(min(self.cornerA.x,self.cornerB.x),min(self.cornerA.y,self.cornerB.y))
-        self.maxs = vector(max(self.cornerA.x,self.cornerB.x),max(self.cornerA.y,self.cornerB.y))
+        self.mins = vector(min(self.cornerA.x, self.cornerB.x), min(self.cornerA.y, self.cornerB.y))
+        self.maxs = vector(max(self.cornerA.x, self.cornerB.x), max(self.cornerA.y, self.cornerB.y))
 
-    def move(self,translationVector):
+    def move(self, translationVector):
         """Translates a side along a given vector"""
-        self.cornerA+=translationVector
-        self.cornerB+=translationVector
+        self.cornerA += translationVector
+        self.cornerB += translationVector
         self.extremesCalc()
--- src/vector.py
+++ src/vector.py
@@ -1,19 +1,19 @@
 class vector:
-    #this class is for making code easier to read
+    """Basic 2D vectors for making more readable code"""
 
-    def __init__(self,x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def __add__(self,other):
-        self.x+=other.x
-        self.y+=other.y
+    def __add__(self, other):
+        self.x += other.x
+        self.y += other.y
 
     def size(self):
         """calculates the hypotemuse of a vector"""
-        return (self.x**2+self.y**2)**0.5
+        return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
-    def distance(vectorA,vectorB):
+    def distance(vectorA, vectorB):
         """calculates the distance between two vectors"""
-       finalVector=vector(abs(vectorA.x-vectorB.x),abs(vectorA.y-vectorB.y))
+       finalVector=vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
        return finalVector.size()
