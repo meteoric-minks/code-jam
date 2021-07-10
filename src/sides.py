615fdb67c8c7093c7065c3c41a24187f4b425124
hecent <henrytchandler@outlook.com>
2021-07-10 15:31:50 +0100

    type hinting

--- src/sides.py
+++ src/sides.py
@@ -4,17 +4,17 @@ from vector import vector
 class side:
     """Side objects to make up the geometry of the world"""
 
-    def __init__(self, cornerA, cornerB):
+    def __init__(self, cornerA: vector(), cornerB: vector()) -> None:
         self.cornerA = cornerA
         self.cornerB = cornerB
         self.extremesCalc()
 
-    def extremesCalc(self):
+    def extremesCalc(self) -> None:
         """Gets the min and max point of the side"""
         self.mins = vector(min(self.cornerA.x, self.cornerB.x), min(self.cornerA.y, self.cornerB.y))
         self.maxs = vector(max(self.cornerA.x, self.cornerB.x), max(self.cornerA.y, self.cornerB.y))
 
-    def move(self, translationVector):
+    def move(self, translationVector: vector()) -> None:
         """Translates a side along a given vector"""
         self.cornerA += translationVector
         self.cornerB += translationVector
--- src/vector.py
+++ src/vector.py
@@ -1,18 +1,20 @@
 class vector:
     """Basic 2D vectors for making more readable code"""
 
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
-    def __add__(self, other):
-        return vector(self.x + other.x, self.y + other.y)
+    def __add__(self, other) -> None:
+        self.x += other.x
+        self.y += other.y
 
-    def size(self):
+    def size(self) -> float:
         """Calculates the hypotemuse of a vector"""
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
-    def distance(vectorA, vectorB):
-        """Calculates the distance between two vectors"""
-        finalVector = vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
-        return finalVector.size()
+
+def distance(vectorA: vector(), vectorB: vector()) -> float:
+    """Calculates the distance between two vectors"""
+    finalVector = vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
+    return finalVector.size()
