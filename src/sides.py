67e23f203866b095fdb69f50e3143d52387f7695
hecent <henrytchandler@outlook.com>
2021-07-10 14:54:33 +0100

    dfgghsfghdfgh

--- src/sides.py
+++ src/sides.py
@@ -1,7 +1,8 @@
 from vector import vector
 
+
 class side:
-    """side objects to make up the geometry of the world"""
+    """Side objects to make up the geometry of the world"""
 
     def __init__(self, cornerA, cornerB):
         self.cornerA = cornerA
@@ -9,7 +10,7 @@ class side:
         self.extremesCalc()
 
     def extremesCalc(self):
-        """gets the min and max point of the side"""
+        """Gets the min and max point of the side"""
         self.mins = vector(min(self.cornerA.x, self.cornerB.x), min(self.cornerA.y, self.cornerB.y))
         self.maxs = vector(max(self.cornerA.x, self.cornerB.x), max(self.cornerA.y, self.cornerB.y))
 
--- src/vector.py
+++ src/vector.py
@@ -10,10 +10,10 @@ class vector:
         self.y += other.y
 
     def size(self):
-        """calculates the hypotemuse of a vector"""
+        """Calculates the hypotemuse of a vector"""
         return ((self.x ** 2) + (self.y ** 2)) ** 0.5
 
     def distance(vectorA, vectorB):
-        """calculates the distance between two vectors"""
-       finalVector=vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
-       return finalVector.size()
+        """Calculates the distance between two vectors"""
+        finalVector = vector(abs(vectorA.x - vectorB.x), abs(vectorA.y - vectorB.y))
+        return finalVector.size()
