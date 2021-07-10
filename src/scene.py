82fccc6b5b80cbd883440ce0ee37dc6b1829b890
hecent <henrytchandler@outlook.com>
2021-07-10 19:18:41 +0100

    started working on class for writing to screen

---
+++ src/scene.py
@@ -0,0 +1,26 @@
+class bar:
+    """The bars that appear on screen"""
+
+    def __init__(self):
+        self.color = None
+        self.texture = None
+        self.distance = None
+
+    def addWall(self, color: tuple, texture: str, distance: float) -> None:
+        """Updates the bar's properties the wall if it's closer than previous walls"""
+        if self.distance is None or self.distance > distance:
+            self.distance = distance
+
+
+# class room:
+#    """Room object"""
+#
+#    def __init__(self):
+#        self.walls = []
+#
+#    def render(self, playerLocation: vector(), playerRotation: float, fov: int, resolution: list) -> None:
+#        """Renders the scene in the terminal"""
+#        frame = [bar() for i in range(resolution[0])]
+#        for eachWall in self.walls:
+#            wallLocation = eachWall.getScreenLocation(playerLocation, playerRotation, fov)
+#            gradient = (wallLocation[0][1] - wallLocation[1][1]) / (wallLocation[0][0] - wallLocation[1][0])
