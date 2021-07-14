bab569f3227ba5ba92a46916270b77fbd556bf08
hecent <henrytchandler@outlook.com>
2021-07-14 12:30:47 +0100

    Forgot to return function results

--- src/ciphers.py
+++ src/ciphers.py
@@ -1,15 +1,17 @@
 from string import ascii_lowercase
 
 
-def shift(character: str, shift: int):
+def shift(character: str, shift: int) -> str:
     """Shifts a single character along the alphabet"""
     character.lower()
     location = ascii_lowercase.index(character)
     character = ascii_lowercase[(location + shift) % 26]
+    return character
 
 
-def caesar(plainText: str, key: int):
+def caesar(plainText: str, key: int) -> str:
     """Performs the caesar chipher on a string given a key"""
     cipherText = ""
     for char in plainText:
         cipherText += shift(char, key)
+    return cipherText
