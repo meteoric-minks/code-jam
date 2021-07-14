67925a2e3799ea8e3f24da6a29ac4fbd0673859c
hecent <henrytchandler@outlook.com>
2021-07-14 15:52:03 +0100

    converted camelCasing to snake_casing

--- src/ciphers.py
+++ src/ciphers.py
@@ -9,6 +9,7 @@ ALPHA_DIGITS = FULL_ALPHA + "0123456789"
 
 def shift(character: str, shift: int) -> str:
     """Shifts a single character along the alphabet"""
+    # Checking if character is lowercase, uppercase or not a letter
     if character in LOWER_ALPHA:
         temp_alpha = LOWER_ALPHA
     elif character in UPPER_ALPHA:
@@ -21,53 +22,53 @@ def shift(character: str, shift: int) -> str:
     return character
 
 
-def caesar(plainText: str, key: int) -> str:
+def caesar(plain_text: str, key: int) -> str:
     """Performs the caesar chipher on a string given a key"""
-    cipherText = ""
-    for char in plainText:
-        cipherText += shift(char, key)
-    return cipherText
+    cipher_text = ""
+    for char in plain_text:
+        cipher_text += shift(char, key)
+    return cipher_text
 
 
-def pairSwapper(plainText: str) -> str:
+def neighbour_swap(plain_text: str) -> str:
     """Swaps adjacent characters in pairs. Eg: \"PYTHON\" > \"YPHTNO\""""
-    plainText = list(plainText)
+    plain_text = list(plain_text)
     symbols = []
 
     # removing symbols and storing their location
-    for index in range(len(plainText)):
-        if not(plainText[index] in ALPHA_DIGITS):
-            symbols.append([index, plainText[index]])
-            plainText[index] = "@"  # maintains string length so subsequent loops aren't messed up
+    for index in range(len(plain_text)):
+        if not(plain_text[index] in ALPHA_DIGITS):
+            symbols.append([index, plain_text[index]])
+            plain_text[index] = "@"  # maintains string length so subsequent loops aren't messed up
 
-    plainText = [char for char in plainText if char != "@"]  # removing the "@"s
+    plain_text = [char for char in plain_text if char != "@"]  # removing the "@"s
 
-    for index in range(1, len(plainText), 2):  # Goes through every pair of characters
+    for index in range(1, len(plain_text), 2):  # Goes through every pair of characters
         # Assigns both simultaneously as both read from eachother
-        plainText[index - 1], plainText[index] = plainText[index], plainText[index - 1]
+        plain_text[index - 1], plain_text[index] = plain_text[index], plain_text[index - 1]
 
     # Adding symbols back to final string
     for element in symbols:
-        plainText.insert(element[0], element[1])
+        plain_text.insert(element[0], element[1])
 
-    return "".join(plainText)
+    return "".join(plain_text)
 
 
-def randomSubstitute(plainText: str, seed: int) -> str:
-    """Encodes a string using a randomly generated substitution cipher (the same seed will use the same cipher)"""
-    lowerShuffle = list(LOWER_ALPHA)
-    upperShuffle = list(UPPER_ALPHA)
+def random_substitution(plain_text: str, seed: int) -> str:
+    """Encodes a string using a substitution cipher generated from the seed"""
+    lower_shuffle = list(LOWER_ALPHA)
+    upper_shuffle = list(UPPER_ALPHA)
     random.seed(seed)  # makes randomization always the same for a specific seed
-    random.shuffle(lowerShuffle)
+    random.shuffle(lower_shuffle)
     random.seed(seed)
-    random.shuffle(upperShuffle)
+    random.shuffle(upper_shuffle)
     # Dictionary for encoding the string, eg. {"a": "u", "b": "e" ...}
     # zip() is used to combine two iterables into one
-    cipher = {letter: randLetter for letter, randLetter in zip(FULL_ALPHA, lowerShuffle + upperShuffle)}
-    cipherText = ""
-    for char in plainText:
+    cipher = {letter: randLetter for letter, randLetter in zip(FULL_ALPHA, lower_shuffle + upper_shuffle)}
+    cipher_text = ""
+    for char in plain_text:
         if char in list(FULL_ALPHA):
-            cipherText += cipher[char]
+            cipher_text += cipher[char]
         else:
-            cipherText += char
-    return cipherText
+            cipher_text += char
+    return cipher_text
