00075cf0a243855f31d5a507297279bb028285f9
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2021-07-16 13:10:03 +0100

    Add some drawing characters to ascii_box

--- src/application/ascii_box.py
+++ src/application/ascii_box.py
@@ -55,3 +55,14 @@ class Double(LineChar):
     DownHorizontal = '╦'
     UpHorizontal = '╩'
     VerticalHorizontal = '╬'
+
+
+class DrawingChar(Enum):
+    """Characters for drawing things, other than lines."""
+
+    def __str__(self):
+        """Printing line character prints its value"""
+        return self.value
+
+    Box = '▣'
+    Vase = '⚱'
