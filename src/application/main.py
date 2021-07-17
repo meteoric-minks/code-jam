9152a2fada513dbb917fe72c4bd5486dc88c0111
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2021-07-17 14:08:57 +0100

    add more rooms

--- src/application/main.py
+++ src/application/main.py
@@ -8,28 +8,44 @@ from application.dungeon import Character, Dungeon, Item, Room
 from application.menu import Button, DungeonEngine, Menu, TextUI
 
 
+def setup_dungeon() -> Dungeon:
+    """Setup the dungeon and rooms"""
+    item0 = Item(1, 1, DrawingChar.Vase)
+    item1 = Item(4, 2, DrawingChar.Box)
+    item2 = Item(8, 1, DrawingChar.Vase)
+
+    Hroom0 = Room(0, 0, width=16, c=Heavy)
+    Hroom0.add_item(item0)
+    Hroom0.add_item(item1)
+
+    Hroom1 = Room(15, 0, width=12, height=4, c=Heavy)
+
+    Hroom2 = Room(24, 3, c=Heavy)
+
+    Droom0 = Room(15, 3, c=Double)
+    Droom0.add_item(item2)
+
+    Droom1 = Room(24, 6, width=30, height=3, c=Double)
+
+    dung = Dungeon()
+    dung.add_room(Hroom0)
+    dung.add_room(Hroom1)
+    dung.add_room(Hroom2)
+    dung.add_room(Droom0)
+    dung.add_room(Droom1)
+
+    char = Character(dung, 2, 2, c=DrawingChar.AltCharacter)
+    dung.set_character(char)
+
+    return dung
+
+
 def show_menu() -> None:
     """Shows a menu with start and end buttons. Start button shows a message box"""
     term = Terminal()
     tui = TextUI(term)
 
-    item0 = Item(1, 1, DrawingChar.Vase)
-    item1 = Item(4, 2, DrawingChar.Box)
-    item2 = Item(8, 1, DrawingChar.Vase)
-
-    room0 = Room(0, 0, c=Heavy)
-    room0.add_item(item0)
-    room0.add_item(item1)
-
-    room1 = Room(10, 3, c=Double)
-    room1.add_item(item2)
-
-    dung = Dungeon()
-    dung.add_room(room0)
-    dung.add_room(room1)
-
-    char = Character(dung, 2, 2, c=DrawingChar.AltCharacter)
-    dung.set_character(char)
+    dung = setup_dungeon()
 
     dung_wrapper = DungeonEngine(tui, dung)
 
